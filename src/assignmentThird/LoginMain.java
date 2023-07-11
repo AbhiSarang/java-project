8ab63e2c5c42702f2f4662c903552c8fe3206154
AbhiSarang <abhi.singhdnn@gmail.com>
2023-07-10 22:47:58 -0400

    adding comment

--- src/assignmentThird/LoginMain.java
+++ src/assignmentThird/LoginMain.java
@@ -5,21 +5,17 @@ import java.util.Scanner;
 public class LoginMain {
 
 	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
+
 		LoginPage L1 = new LoginPage();
+		Scanner sc = new Scanner(System.in);
 		L1.userName = "pivotAdmin";
 		System.out.println("Enter the usename:");
 		L1.EnteredUserName = sc.nextLine();
-		
 		L1.password = "Admin123";
 		System.out.println("Enter password:");
 		L1.EnteredPassword = sc.nextLine();
 		L1.loginClass();
 		sc.close();
-		
 
 	}
-
 }
--- src/assignmentThird/LoginPage.java
+++ src/assignmentThird/LoginPage.java
@@ -1,32 +1,44 @@
 package assignmentThird;
 
+import java.util.Scanner;
+
 public class LoginPage {
-	String userName = "pivotAdmin";;
-	String password = "Admin123";;
+	String userName = "pivotAdmin";
+	String password = "Admin123";
 	String EnteredUserName;
 	String EnteredPassword;
-	int maxAttempts = 3;
+	int maxAttempts = 2;
 
 	void loginClass()
 
 	{
+		Scanner sc = new Scanner(System.in);
 		int attempts = 0;
 
 		while (attempts < maxAttempts) {
 			if (EnteredUserName.equals(userName) && EnteredPassword.equals(password)) {
-
 				System.out.println("you are logged in to the application");
 				break;
 			} else {
 				attempts++;
+				System.out.println("Incorrect username or password. Try Again");
 				int remainingAttempts = maxAttempts - attempts;
-				System.out.println("incorrect username or password " + remainingAttempts);
+				System.out.println("Enter the usename:");
+				EnteredUserName = sc.nextLine();
+				System.out.println("Enter password:");
+				EnteredPassword = sc.nextLine();
+				if (EnteredUserName.equals(userName) && EnteredPassword.equals(password)) {
+					System.out.println("you are logged in to the application");
+					break;
+				}
+				System.out.println("Remaining attempts left: " + remainingAttempts);
 
 			}
-			if (attempts == 0) {
-				System.out.println("account locked");
+			if (attempts == 2) {
+				System.out.println("Maximum attempts reached. Account is locked");
 			}
 		}
+		
 	}
 
 }
